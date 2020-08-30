5a3b0a5c00153f9901e3189d0765a11a74824d33
Zeke Mostov <z.mostov@gmail.com>
2020-08-30 12:47:19 -0700

    Create example bug of derive.chain.getHeader()

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,30 @@
 import { ApiPromise, WsProvider } from '@polkadot/api';
 
 async function main(): Promise<void> {
-	const wsProvider = new WsProvider('wss://cc3-5.kusama.network');
+	const wsProvider = new WsProvider('wss://rpc.polkadot.io');
 	const api = await ApiPromise.create({
 		provider: wsProvider,
 	});
 
-	// Kusama block 900
-	const hash =
-		'0xc6ec2b15cc9436d9b494e917511f283b29d337984d858e4324aad452d8265aa1';
+	const hash1378238 =
+		'0xbc38431a6a4e0cdaadd3b34d1fd1d6692cb08ff07d58bb1726101d341f100892'; // polkadot block number 1378238
+	const firstTry1378238 = await api.derive.chain.getHeader(hash1378238);
+	console.log('This works: (firstTry1378238) ', firstTry1378238?.toHuman());
 
-	const block = await api.rpc.chain.getBlock(hash);
+	const secondTry1378238 = await api.derive.chain.getHeader(hash1378238);
+	console.log('This works: (secondTry1378238) ', secondTry1378238?.toHuman());
 
+	const hash1 =
+		'0xc0096358534ec8d21d01d34b836eed476a1c343f8724fa2153dc0725ad797a90'; // polkadot block number 1
+
+	const firstTry1 = await api.derive.chain.getHeader(hash1);
+	console.log('This works: (firstTry1)', firstTry1?.toHuman());
+
+	const secondTry1 = await api.derive.chain.getHeader(hash1);
+	// Logs to console: Unable to decode Vec on index 0 Tuple: failed on 0:: Invalid character
 	console.log(
-		block.block.extrinsics.forEach((e) => console.log(e.toHuman()))
+		'This will not log to console (secondTry1)',
+		secondTry1?.toHuman()
 	);
 }
 
